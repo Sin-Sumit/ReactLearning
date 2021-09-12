a3686c6d6e0d14ed0e0e2f442b4b0ea9d5bf3563
Sumit Singh <sinsumit13@gmail.com>
2021-09-12 15:00:55 +0530

    Contains items object

---
+++ src/list.js
@@ -0,0 +1,27 @@
+// Creating a JAVASCRIPT OBJECT which is used for iterating the values and for creating a list of object use array.
+export const values=[
+    {   
+        id: 1,
+        name: 'Sandeep',
+        age: 29,
+        height: 186
+    },
+    {
+        id: 2,
+        name: 'Sumit',
+        age: 22,
+        height: 180
+    },
+    {
+        id: 3,
+        name: 'Suman',
+        age: 52,
+        height: 182
+    },
+   {
+        id: 4,
+        name: 'Ashok',
+        age: 55,
+        height: 189
+    },
+];
