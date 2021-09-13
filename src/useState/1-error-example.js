03e50db58d991a68be0be05a7b394f10ede65897
Sumit Singh <sinsumit13@gmail.com>
2021-09-13 12:11:42 +0530

    Create 1-error-example.js

---
+++ src/useState/1-error-example.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ErrorExample = () => {
+let title = 'Random Title';
+const handleClick = () => {
+  title = 'Hello World!!';
+};
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+      Change Title</button>
+    </React.Fragment>
+  );
+};
+
+export default ErrorExample;
