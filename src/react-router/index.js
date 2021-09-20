fc1662b89494ad8d96d438b7f0513c7b39d09d48
Sumit Singh <sinsumit13@gmail.com>
2021-09-20 16:50:30 +0530

    Create index.js

---
+++ src/react-router/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// react router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// pages
+import Home from './Home';
+import About from './About';
+import People from './People';
+import Error from './Error';
+import Person from './Person';
+// navbar
+import Navbar from './Navbar';
+const ReactRouterSetup = () => {
+  return (<Router>
+  <Navbar />
+  <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/people'>
+        <People />
+      </Route>
+    </Switch>
+  </Router>);
+};
+
+export default ReactRouterSetup;
